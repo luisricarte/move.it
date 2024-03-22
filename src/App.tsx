35f0a5d88c839fa4315c8d15023756234a3177b9
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-03-21 22:42:47 -0300

    adicionando estrutura de fonts globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { Button } from "./components/Button";
+import './styles/global.css';
 
 function App() {
   return (
     <>    
-    <Button color='gray'>botão 1</Button>
-    <Button>botão 2</Button>
     <h1>Hello World</h1></>
 
   );
