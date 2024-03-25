09a7f51ae1594effcbe0fa2f5b4e425eb11012dd
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-03-25 00:04:53 -0300

    adicionando estilo no componente de profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,6 +1,12 @@
 import styles from './styles.module.css';
-export default function Profile() {
+export function Profile() {
     return (
-        <h1>profile</h1>
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/luisricarte.png" alt="Profile Photo" />
+            <div>
+                <strong>Luis Eduardo Ricarte</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { ExperienceBar } from "@/components/ExperienceBar";
+import { Profile } from "@/components/Profile";
 import Head from "next/head";
-
+import styles from '../styles/components/Home.module.css';
 export default function Home() {
     return(
-        <div className='container'>    
+        <div className={styles.container}>    
 
             <ExperienceBar/>
+            <section>
+                <div>
+                    <Profile/>
+                </div>
+                <div></div>
+            </section>
         </div>
     
     )
