83c13008db5d4a3f93452146d7fa3e6cf9db56a8
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-03-28 23:00:30 -0300

    adicionando inicio do context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/global.css'
-
+import { challegensContext } from '../contexts/ChallengesContext';
 function MyApp({ Component, pageProps}) {
-    return <Component {...pageProps}/>
+    
+    return(
+        <>
+        <challegensContext.Provider>
+            <Component {...pageProps}/>
+        </challegensContext.Provider>
+        </>
+    ) 
 }
 
 export default MyApp
