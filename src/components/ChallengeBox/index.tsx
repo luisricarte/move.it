fa23a898ec6063f38a9fb4c2ceca07fe7ade36ed
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-05-05 01:48:54 -0300

    adicionando XP mock e desafio ativo

--- src/components/ChallengeBox/index.tsx
+++ src/components/ChallengeBox/index.tsx
@@ -1,24 +1,30 @@
 import { Button } from '../Button';
 import styles from './styles.module.css';
 import stylesButton from '../Button/styles.module.css';
+import { useContext } from 'react';
+import { ChallengesContext } from '@/contexts/ChallengesContext';
 export function ChallengeBox() {
+
+    const { activeChallenge, resetChallenge } = useContext(ChallengesContext)
+
     const hasActiveChallenge = true
     return (
         <div className={styles.challengeBoxContainer}>
             {
-                hasActiveChallenge ? (
+                activeChallenge ? (
                     <div className={styles.challengeActive}>
-                        <header>Ganhe 400xp</header>
+                        <header>{`Ganhe ${activeChallenge.amount}xp`}</header>
 
                         <main>
-                            <img src="icons/body.svg" alt="" />
+                            <img src={`icons/${activeChallenge.type}.svg`} alt="" />
                             <strong>Novo desafio</strong>
-                            <p>Levante e faça uma caminhada de 3 minutos</p>
+                            <p>{activeChallenge.description}</p>
                         </main>
                         <footer>
                             <Button
                                 type='button'
                                 className={stylesButton.challengeFailedButton}
+                                onClick={resetChallenge}
                             >
                                 Falhei
                             </Button>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,10 +1,20 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
+
+interface ChallengeMockProps {
+    type: 'body' | 'eye' | string,
+    description: string,
+    amount: number
+}
+
 interface ChallengesContextData {
     level: number,
     currentExperience: number;
     challengesCompleted: number;
     levelUp: () => void;
     startNewChallenge: () => void;
+    activeChallenge: ChallengeMockProps | undefined;
+    resetChallenge: () => void;
 }
 
 type ChallengesProviderProps = {
@@ -17,13 +27,20 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
     const [level, setLevel] = useState(1);
     const [currentExperience, setCurrentExperience] = useState(0);
     const [challengesCompleted, setChallengesCompleted] = useState(0);
+    const [activeChallenge, setActiveChallenge] = useState<ChallengeMockProps>() 
 
     function levelUp(){ 
         setLevel(level + 1);
     }
 
     function startNewChallenge() {
-        console.log('New Challenge');
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+        setActiveChallenge(challenge);
+    }
+
+    function resetChallenge () {
+        setActiveChallenge({type:'',description:'', amount:0});
     }
     return (
 
@@ -33,7 +50,9 @@ export function ChallengesProvider({children}: ChallengesProviderProps) {
                 levelUp, 
                 currentExperience, 
                 challengesCompleted, 
-                startNewChallenge}}>
+                startNewChallenge,
+                activeChallenge,
+                resetChallenge}}>
             {children}
         </ChallengesContext.Provider>
     )
