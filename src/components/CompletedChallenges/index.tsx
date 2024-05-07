ffbb78504b88ebc1a39fb64316ac799ea79f210c
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-05-06 22:47:05 -0300

    adicionando desafios completos

--- src/components/CompletedChallenges/index.tsx
+++ src/components/CompletedChallenges/index.tsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react';
 import styles from './styles.module.css';
+import { ChallengesContext } from '@/contexts/ChallengesContext';
 
 export function CompletedChallenges() {
+    const {challengesCompleted} = useContext(ChallengesContext)
+
     return( 
         <div className={styles.completedChallengesContainer}>
             <span>
                 Desafios Completos:
             </span>
-            <span>05</span>
+            <span>{challengesCompleted}</span>
         </div>
     )
 }
