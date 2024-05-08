c28c2327f7c450f8228ee2e9f7d9e79463452e60
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-05-07 22:13:01 -0300

    adicionando countdown context

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -3,44 +3,14 @@ import { Button } from '../Button';
 import styles from './styles.module.css';
 import stylesButton from '../Button/styles.module.css';
 import { ChallengesContext } from '@/contexts/ChallengesContext';
-
-let countdownTimeout: NodeJS.Timeout;
+import { CountdownContext, CountdownProvider } from '@/contexts/CountdownContext';
 
 export function Countdown() {
-    const {startNewChallenge} = useContext(ChallengesContext);
-
-    const [count,setCount] = useState(0.1 * 60);
-    const [isActive, setIsActive] = useState(false);
-    const [hasFinished, setHasFinished] = useState(false);
-
-    const minutes = Math.floor(count / 60);
-    const seconds = count % 60;
-
+    const { minutes, seconds, hasFinished, isActive, resetCountDown, startCountDown } = useContext(CountdownContext);
     const [minuteLeft, minuteRight] = String(minutes).padStart(2,'0').split('');
     const [secondLeft, secondRight] = String(seconds).padStart(2,'0').split('');
     
-    function startCountDown() {
-        setIsActive(true);
-    }
 
-    function cancelCountDown() {
-        clearTimeout(countdownTimeout);
-        setCount(0.1 * 60);
-        setIsActive(false);
-    }
-
-    useEffect(()=> {
-        if (isActive && count > 0) {
-            countdownTimeout = setTimeout(()=> {
-                setCount(count - 1)
-            }, 1000)
-        }
-        else if ( count == 0 ) {
-            setHasFinished(true);
-            setIsActive(false);
-            startNewChallenge();
-        }
-    }, [isActive,count])
 
     return (
         <div>
@@ -68,7 +38,7 @@ export function Countdown() {
                 ) :
                 (
                     <Button
-                        onClick={isActive ?  cancelCountDown : startCountDown}
+                        onClick={isActive ?  resetCountDown : startCountDown}
                         className={ `${stylesButton.countDown} ${isActive ? stylesButton.countDownActive : stylesButton.countDownUnactive}`}   
                     >
                     { isActive ? 'Abandonar Ciclo ' : 'Iniciar um Ciclo'}
--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -1,21 +1,65 @@
-import { ReactNode, createContext } from "react";
+import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import { ChallengesContext } from "./ChallengesContext";
 
 interface CountdownContextData {
-
+    minutes: number;
+    seconds: number;
+    hasFinished: boolean;
+    isActive: boolean;
+    startCountDown: ()=>void;
+    resetCountDown: ()=>void;
 }
 
 interface CountdownProviderProps {
     children: ReactNode;
 }
 
-const CountdownContext = createContext({} as CountdownContextData);
+let countdownTimeout: NodeJS.Timeout;
+
+export const CountdownContext = createContext({} as CountdownContextData);
 
 export function CountdownProvider({children}: CountdownProviderProps) {
+    const {startNewChallenge} = useContext(ChallengesContext);
 
-    
-    
+    const [count,setCount] = useState(0.1 * 60);
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
+
+    const minutes = Math.floor(count / 60);
+    const seconds = count % 60;
+
+    function startCountDown() {
+        setIsActive(true);
+    }
+
+    function resetCountDown() {
+        clearTimeout(countdownTimeout);
+        setCount(0.1 * 60);
+        setIsActive(false);
+    }
+
+    useEffect(()=> {
+        if (isActive && count > 0) {
+            countdownTimeout = setTimeout(()=> {
+                setCount(count - 1)
+            }, 1000)
+        }
+        else if ( count == 0 ) {
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
+        }
+    }, [isActive,count])
+
     return (
-        <CountdownContext.Provider value={{}}>
+        <CountdownContext.Provider value={{
+            minutes,
+            seconds,
+            hasFinished,
+            isActive,
+            startCountDown,
+            resetCountDown
+        }}>
             {children}
         </CountdownContext.Provider>
     )
