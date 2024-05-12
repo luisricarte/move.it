8033286807ba5061cbd08b5be8e37f7c06d94993
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-05-11 23:50:46 -0300

    adicionando persistência das informações dos cookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,41 +7,57 @@ import Head from "next/head";
 import styles from '../styles/components/Home.module.css';
 import { ChallengeBox } from "@/components/ChallengeBox";
 import { CountdownProvider } from "@/contexts/CountdownContext";
+import { ChallengesProvider } from "@/contexts/ChallengesContext";
 
+interface HomeProps{
+    level: number,
+    currentExperience: number,
+    challengesCompleted: number,
+}
 
-export default function Home() {
+export default function Home(props: HomeProps) {
+    console.log(props);
     return(
-        <div>
-        <div className={styles.container}>    
-            <Head>
-                <title>Inicio | Move.it</title>
-            </Head>
+        <ChallengesProvider 
+            level={props.level} 
+            currentExperience={props.currentExperience} 
+            challengesCompleted={props.challengesCompleted}
+        >
+                <div className={styles.container}>    
+                    <Head>
+                        <title>Inicio | Move.it</title>
+                    </Head>
 
-            <ExperienceBar/>
+                    <ExperienceBar/>
 
-            <CountdownProvider>
-                <section>
-                    <div>
-                        <Profile/>
-                        <CompletedChallenges/>
-                        <Countdown/>
-                        
-                    </div>
-                    <div>
-                        <ChallengeBox/>
-                    </div>
-                </section>
-            </CountdownProvider>
-        </div>
-
-        
-        </div>
+                    <CountdownProvider>
+                        <section>
+                            <div>
+                                <Profile/>
+                                <CompletedChallenges/>
+                                <Countdown/>
+                                
+                            </div>
+                            <div>
+                                <ChallengeBox/>
+                            </div>
+                        </section>
+                    </CountdownProvider>
+                </div>
+        </ChallengesProvider>
     )
 }
 
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+    const {level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
     return {
-        props: {}
+        props: {
+            level: Number(level),
+            currentExperience: Number(currentExperience),
+            challengesCompleted: Number(challengesCompleted)
+        }
     }
 }
