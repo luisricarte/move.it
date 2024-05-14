729a88cce5bb48003c0b9cb924d9e6ee1e1e9bb1
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-05-14 20:15:29 -0300

    ajustando tempo de pomodoro

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 export function CountdownProvider({children}: CountdownProviderProps) {
     const {startNewChallenge} = useContext(ChallengesContext);
 
-    const [count,setCount] = useState(0.1 * 60);
+    const [count,setCount] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
